368a7736fd857aa1434c10d10013f073c6ef1cb5
Sam Ford <1584702+samford@users.noreply.github.com>
2026-03-23 14:37:44 -0400

    cmd/search: resolve error with regex
    When a regex string like `/jq/` is provided to `brew search`, it can
    sometimes produce an error like "Error: /opt/homebrew/opt//jq/ is not
    a valid keg". So far I've only seen this with a very small number of
    installed formulae (I only stumbled upon this by chance). I'm not
    sure why some regexes trigger this and others don't but it traces
    back to the `Formulary.path` call in `MissingFormula.deleted_reason`.
    Either way, `MissingFormula.reason` shouldn't be called when the
    `query` value is a regex string, so this adds a guard to prevent it.

--- Library/Homebrew/test/cmd/search_spec.rb
+++ Library/Homebrew/test/cmd/search_spec.rb
@@ -13,4 +13,44 @@ RSpec.describe Homebrew::Cmd::SearchCmd do
       .to output(/testball/).to_stdout
       .and be_a_success
   end
+
+  describe "::print_missing_formula_help" do
+    let(:search_cmd) { described_class.new([""]) }
+
+    context "when $stdout is not a TTY" do
+      before { allow_any_instance_of(StringIO).to receive(:tty?).and_return(false) }
+
+      it "skips" do
+        expect { search_cmd.send(:print_missing_formula_help, "formula", false) }
+          .not_to output.to_stdout
+      end
+    end
+
+    context "when $stdout is a TTY" do
+      before { allow_any_instance_of(StringIO).to receive(:tty?).and_return(true) }
+
+      it "skips a regex query" do
+        expect { search_cmd.send(:print_missing_formula_help, "/formula/", false) }
+          .not_to output.to_stdout
+      end
+
+      it "skips if there is not a reason" do
+        allow(Homebrew::MissingFormula).to receive(:reason).and_return(nil)
+        expect { search_cmd.send(:print_missing_formula_help, "formula", false) }
+          .not_to output.to_stdout
+      end
+
+      it "prints additional output if `found_matches` is true" do
+        allow(Homebrew::MissingFormula).to receive(:reason).and_return("Reason")
+        expect { search_cmd.send(:print_missing_formula_help, "formula", true) }
+          .to output("\nIf you meant \"formula\" specifically:\nReason\n").to_stdout
+      end
+
+      it "only prints reason if `found_matches` is false" do
+        allow(Homebrew::MissingFormula).to receive(:reason).and_return("Reason")
+        expect { search_cmd.send(:print_missing_formula_help, "formula", false) }
+          .to output("Reason\n").to_stdout
+      end
+    end
+  end
 end
