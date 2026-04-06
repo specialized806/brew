966df6077b72eb2f2c428b4dbc84eb17e089ade5
hyuraku <32809703+hyuraku@users.noreply.github.com>
2026-04-06 21:40:37 +0900

    refactor: remove TODO comment about Xcode license check speed

--- Library/Homebrew/brew.sh
+++ Library/Homebrew/brew.sh
@@ -876,7 +876,6 @@ EOS
   # a popup window asking the user to install the CLT
   if [[ -n "${XCODE_SELECT_PATH}" ]]
   then
-    # TODO: this is fairly slow, figure out if there's a faster way.
     XCRUN_OUTPUT="$(/usr/bin/xcrun --find clang 2>&1)"
     XCRUN_STATUS="$?"
 
