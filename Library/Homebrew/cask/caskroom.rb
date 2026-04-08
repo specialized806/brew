0c370b32e0dbb31596a90ed479198a72473bf13a
Kristján Oddsson <hi@koddsson.com>
2026-04-08 11:04:41 +0000

    update other caskfile callsites
    Co-authored-by: Kristján Oddsson <hi@koddsson.com>

--- Library/Homebrew/cask/caskroom.rb
+++ Library/Homebrew/cask/caskroom.rb
@@ -39,14 +39,14 @@ module Cask
     # Return tokens for Caskroom directories missing expected installed metadata.
     sig { returns(T::Array[String]) }
     def self.corrupt_cask_dirs
-      paths.filter_map { |p| p.basename.to_s if caskfile?(p) }
+      paths.filter_map { |p| p.basename.to_s if cask_with_metadata?(p) }
     end
 
     sig { params(cask_path: Pathname).returns(T::Boolean) }
     def self.cask_with_metadata?(cask_path)
       Pathname.glob(cask_path/".metadata"/"*"/"*"/"Casks"/"*.{rb,json}").any?
     end
-    private_class_method :caskfile?
+    private_class_method :cask_with_metadata?
 
     sig { void }
     def self.ensure_caskroom_exists
