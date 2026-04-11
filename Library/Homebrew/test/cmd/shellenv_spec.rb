25984a503e4184b1063f6adc7a909b61fe216665
Sumanth Ratna <sumanthratna@gmail.com>
2026-04-11 16:33:28 -0400

    shellenv: revert test change per review feedback

--- Library/Homebrew/test/cmd/shellenv_spec.rb
+++ Library/Homebrew/test/cmd/shellenv_spec.rb
@@ -3,8 +3,8 @@
 
 RSpec.describe "brew shellenv", type: :system do
   it "prints export statements", :integration_test do
-    expect { brew_sh "shellenv", "zsh", "HOMEBREW_PATH" => "/usr/bin:/usr/sbin" }
-      .to output(/export FPATH/).to_stdout
+    expect { brew_sh "shellenv" }
+      .to output(/.*/).to_stdout
       .and not_to_output.to_stderr
       .and be_a_success
   end
