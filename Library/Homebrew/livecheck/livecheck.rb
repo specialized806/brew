67a039a1143e05f51d44337eaa706ef75bb4a4b9
Sam Ford <1584702+samford@users.noreply.github.com>
2026-04-21 18:28:14 -0400

    livecheck: restore throttle days check
    The `elsif livecheck_throttle_days && throttle_interval_elapsed?(...)`
    condition in the livecheck throttle logic was modified to only use
    `throttle_allows_bump?` with a `throttle_days` argument. However, this
    approach can erroneously set the latest version as `latest_throttled`
    if `throttle_rate` is set, `throttle_days` is not set, and
    `throttled_match_version_map` is blank (i.e., no versions meet the
    throttle conditions). This occurs because the `throttle_allows_bump?`
    call in this `elsif` condition will return true when `throttle_rate`
    is omitted and `throttle_days` is nil, so the `return true if
    throttle_rate.nil? && throttle_days.nil?` early return is used. In
    this scenario, `latest_throttled` should be nil but the `elsif`
    condition prevents the `else` branch from being reached.
    
    This restores the previous `livecheck_throttle_days &&` part of the
    `elsif` condition to ensure that `throttle_allows_bump?` is only
    called with `throttle_days` when it isn't nil. This allows the `else`
    branch to be reached again and ensures throttling in livecheck works
    the same as before.

--- Library/Homebrew/livecheck/livecheck.rb
+++ Library/Homebrew/livecheck/livecheck.rb
@@ -803,7 +803,8 @@ module Homebrew
             version_info[:latest_throttled] = Version.new(
               throttled_match_version_map.values.max_by { |v| LivecheckVersion.create(formula_or_cask, v) },
             )
-          elsif throttle_allows_bump?(formula_or_cask, version_info[:latest], throttle_days: livecheck_throttle_days)
+          elsif livecheck_throttle_days &&
+                throttle_allows_bump?(formula_or_cask, version_info[:latest], throttle_days: livecheck_throttle_days)
             version_info[:latest_throttled] = version_info[:latest]
           else
             version_info[:latest_throttled] = nil
