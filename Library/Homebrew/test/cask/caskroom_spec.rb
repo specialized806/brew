61c0c76b5d67f5dd82558007c9a3ae28c4155065
Mike McQuaid <mike@mikemcquaid.com>
2026-05-02 12:33:34 +0100

    Avoid redundant Caskroom chgrp
    - Check the `Caskroom` group before running `chgrp`.
    - Share the expected group name between checks and fixes.
    - Keep Linux using `sudo` when the group must change.
    
    Fixes https://github.com/Homebrew/brew/issues/22120

--- Library/Homebrew/test/cask/caskroom_spec.rb
+++ Library/Homebrew/test/cask/caskroom_spec.rb
@@ -4,6 +4,55 @@
 require "cask/caskroom"
 
 RSpec.describe Cask::Caskroom do
+  describe ".ensure_caskroom_exists" do
+    it "changes the group when sudo is unnecessary and the group is wrong" do
+      Dir.mktmpdir do |dir|
+        path = Pathname(dir)/"Caskroom"
+        allow(described_class).to receive(:path).and_return(path)
+        allow(described_class).to receive(:caskroom_group_correct?).with(path).and_return(false)
+        expect(described_class).to receive(:chgrp_path).with(path, false)
+
+        described_class.ensure_caskroom_exists
+      end
+    end
+
+    it "skips changing the group when it is already correct" do
+      Dir.mktmpdir do |dir|
+        path = Pathname(dir)/"Caskroom"
+        allow(described_class).to receive(:path).and_return(path)
+        allow(described_class).to receive(:caskroom_group_correct?).with(path).and_return(true)
+        expect(described_class).not_to receive(:chgrp_path)
+
+        described_class.ensure_caskroom_exists
+      end
+    end
+  end
+
+  describe ".caskroom_group_correct?" do
+    it "checks the admin group on macOS", :needs_macos do
+      path = Pathname("/tmp/Caskroom")
+      allow(path).to receive(:stat).and_return(instance_double(File::Stat, gid: 1))
+      allow(Etc).to receive(:getgrnam).with("admin").and_return(instance_double(Etc::Group, gid: 1))
+
+      expect(described_class.caskroom_group_correct?(path)).to be true
+    end
+
+    it "checks the root group on Linux", :needs_linux do
+      path = Pathname("/tmp/Caskroom")
+      allow(path).to receive(:stat).and_return(instance_double(File::Stat, gid: 1))
+      allow(Etc).to receive(:getgrnam).with("root").and_return(instance_double(Etc::Group, gid: 1))
+
+      expect(described_class.caskroom_group_correct?(path)).to be true
+    end
+
+    it "returns false when the expected group is unavailable" do
+      allow(described_class).to receive(:expected_caskroom_group).and_return("missing")
+      allow(Etc).to receive(:getgrnam).with("missing").and_return(nil)
+
+      expect(described_class.caskroom_group_correct?(Pathname("/tmp/Caskroom"))).to be false
+    end
+  end
+
   describe ".corrupt_cask_dirs" do
     it "returns tokens for directories without valid caskfiles" do
       Dir.mktmpdir do |dir|
