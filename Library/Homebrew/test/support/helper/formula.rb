af95141855fb0cf29538fcea7fdfa4db54bcb2f0
Issy Long <me@issylong.com>
2026-05-31 21:30:09 +0100

    sorbet: Add RBI shims for the remainder of the test helper methods
    - And run `brew tc --update --suggest-typed` to confirm success.

--- Library/Homebrew/test/support/helper/formula.rb
+++ Library/Homebrew/test/support/helper/formula.rb
@@ -10,6 +10,12 @@ module Test
 
       requires_ancestor { RSpec::Mocks::ExampleMethods }
 
+      sig {
+        params(
+          name: String, path: T.nilable(Pathname), spec: Symbol, alias_path: T.nilable(Pathname),
+          tap: T.nilable(Tap), block: T.nilable(T.proc.bind(Formula).void)
+        ).returns(::Formula)
+      }
       def formula(name = "formula_name", path: nil, spec: :stable, alias_path: nil, tap: nil, &block)
         path ||= Formulary.find_formula_in_tap(name, tap || CoreTap.instance)
         Class.new(::Formula, &block).new(name, path, spec, alias_path:, tap:)
