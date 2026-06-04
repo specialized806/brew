f8fa491b2c669a4621caaf0581831fb56a7aea09
botantony <antonsm21@gmail.com>
2026-06-04 14:36:50 +0200

    pathname: add testcase for `env_script_all_files`
    Signed-off-by: botantony <antonsm21@gmail.com>

--- Library/Homebrew/test/extend/pathname_spec.rb
+++ Library/Homebrew/test/extend/pathname_spec.rb
@@ -25,4 +25,38 @@ RSpec.describe Pathname do
       end
     end
   end
+
+  describe ".env_script_all_files" do
+    it "create scipts for files" do
+      mktmpdir do |input_dir|
+        FileUtils.touch input_dir/"foo"
+        FileUtils.touch input_dir/"bar"
+
+        mktmpdir do |output_dir|
+          input_dir.env_script_all_files(output_dir, FOO: "foo", BAR: input_dir/"test")
+
+          expect((input_dir/"foo").read).to eq(<<~BASH)
+            #!/bin/bash
+            FOO="foo" BAR="#{input_dir}/test" exec "#{output_dir}/foo"  "$@"
+          BASH
+
+          expect((input_dir/"bar").read).to eq(<<~BASH)
+            #!/bin/bash
+            FOO="foo" BAR="#{input_dir}/test" exec "#{output_dir}/bar"  "$@"
+          BASH
+        end
+      end
+    end
+
+    it "raises an exception when file already exists" do
+      mktmpdir do |input_dir|
+        FileUtils.touch input_dir/"foo"
+
+        mktmpdir do |output_dir|
+          FileUtils.touch output_dir/"foo"
+          expect { input_dir.env_script_all_files(output_dir, FOO: "foo") }.to raise_error(Errno::EEXIST)
+        end
+      end
+    end
+  end
 end
