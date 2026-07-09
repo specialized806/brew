ad20c5f75d073baf95b563367d97bddb7ca91d3b
Patrick Linnane <patrick@linnane.io>
2026-07-08 21:51:09 -0700

    Don't require tap trust in casks/formulae shell specs
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Library/Homebrew/test/cmd/formulae_spec.rb
+++ Library/Homebrew/test/cmd/formulae_spec.rb
@@ -3,7 +3,7 @@
 
 RSpec.describe "brew formulae", type: :system do
   it "prints all installed Formulae", :integration_test do
-    expect { brew_sh "formulae" }
+    expect { brew_sh "formulae", "HOMEBREW_NO_REQUIRE_TAP_TRUST" => "1" }
       .to be_a_success
       .and not_to_output.to_stderr
   end
