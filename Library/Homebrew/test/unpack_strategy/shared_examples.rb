ba83dc80b9fc797fef4e598400fcfc81e8768d32
Michael Cho <michael@michaelcho.dev>
2026-07-12 21:30:13 -0400

    test/unpack_strategy: use Mktemp to set group

--- Library/Homebrew/test/unpack_strategy/shared_examples.rb
+++ Library/Homebrew/test/unpack_strategy/shared_examples.rb
@@ -1,6 +1,7 @@
 # typed: false
 # frozen_string_literal: true
 
+require "mktemp"
 require "unpack_strategy"
 
 RSpec.shared_examples "UnpackStrategy::detect" do
@@ -11,7 +12,8 @@ end
 
 RSpec.shared_examples "#extract" do |children: [], verbose: false|
   specify "#extract" do
-    mktmpdir do |unpack_dir|
+    Mktemp.new("homebrew-test-unpack").run(chdir: false) do |mktemp|
+      unpack_dir = T.must(mktemp.tmpdir)
       described_class.new(path).extract(to: unpack_dir, verbose:)
       expect(unpack_dir.children(false).map(&:to_s)).to match_array children
     end
