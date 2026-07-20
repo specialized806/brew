c653b64ed24e795e7df797ac7b0277547df944cf
Thierry Moisan <thierry.moisan@gmail.com>
2026-07-20 19:17:28 -0400

    fixup! Mark capped PR contribution totals as lower bounds

--- Library/Homebrew/dev-cmd/contributions.rb
+++ Library/Homebrew/dev-cmd/contributions.rb
@@ -191,7 +191,8 @@ module Homebrew
             next if type_count.nil? || type_count.zero?
 
             count_prefix = ""
-            if ((type == :merged_pr_author || type == :merged_pr) && grand_total.fetch(:merged_pr_author_hit_cap, 0).positive?) ||
+            if ([:merged_pr_author, :merged_pr].include?(type) && grand_total.fetch(:merged_pr_author_hit_cap,
+                                                                                    0).positive?) ||
                (type == :approved_pr_review && type_count >= MAX_PR_SEARCH) || type_count >= MAX_CONTRIBUTIONS
               greater_than_total ||= true
               count_prefix = ">="
