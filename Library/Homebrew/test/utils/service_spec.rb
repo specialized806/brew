77e026b88d35fe08ef9ae3980fb8e821c3637805
Ingmar Stein <490610+IngmarStein@users.noreply.github.com>
2026-08-07 14:34:18 +0200

    utils/service: fix `running?` for SSH sessions
    When connected via SSH, `launchctl list <label>` only searches the
    system domain, so it won't find services loaded in the `gui/<uid>`
    domain. This caused `Utils::Service.running?` to incorrectly return
    false for running services, which in turn made `brew upgrade` print
    "brew services start" instead of the correct "brew services restart".
    
    Consolidate the "is this service running?" logic into a single source
    of truth: `Homebrew::Services::System.launchctl_service_running?`.
    This iterates `candidate_domain_targets` to try domain-qualified
    `launchctl print <domain>/<label>` before falling back to a bare
    `launchctl list <label>`. Both `Utils::Service.running?` and
    `FormulaWrapper#loaded?` now use this method.
    
    Refactor `FormulaWrapper#status_output_success_type` to also iterate
    `candidate_domain_targets` instead of only trying the single
    `domain_target`, which over SSH returns `user/<euid>` and misses
    services loaded in the `gui/<uid>` domain.

--- Library/Homebrew/test/utils/service_spec.rb
+++ Library/Homebrew/test/utils/service_spec.rb
@@ -4,6 +4,41 @@
 require "utils/service"
 
 RSpec.describe Utils::Service do
+  describe "::running?" do
+    it "returns false when neither launchctl nor systemctl is available" do
+      f = formula do
+        T.bind(self, T.class_of(Formula))
+        url "foo-1.0"
+      end
+      allow(described_class).to receive_messages(launchctl: nil, systemctl?: false)
+      expect(described_class.running?(f)).to be false
+    end
+
+    it "delegates to System.launchctl_service_running? on macOS" do
+      f = formula do
+        T.bind(self, T.class_of(Formula))
+        url "foo-1.0"
+      end
+      allow(described_class).to receive(:launchctl?).and_return(true)
+      allow(Homebrew::Services::System).to receive(:launchctl_service_running?)
+        .with(f.plist_name).and_return(true)
+      expect(described_class.running?(f)).to be true
+    end
+
+    it "uses systemctl is-active when systemctl is available" do
+      f = formula do
+        T.bind(self, T.class_of(Formula))
+        url "foo-1.0"
+      end
+      allow(described_class).to receive_messages(launchctl: nil, systemctl?: true,
+                                                 systemctl: Pathname("/bin/systemctl"))
+      expect(described_class).to receive(:quiet_system)
+        .with(instance_of(Pathname), "is-active", "--quiet", f.service_name)
+        .and_return(true)
+      expect(described_class.running?(f)).to be true
+    end
+  end
+
   describe "::systemd_quote" do
     it "quotes empty strings correctly" do
       expect(described_class.systemd_quote("")).to eq '""'
