3e27211babf1371daf4612cd56e27e402a5d50ed
AltCode <altcode@vivaldi.net>
2026-08-16 01:18:00 +0200

    Add tests for all conditional stanzas

--- Library/Homebrew/test/rubocops/cask/empty_conditional_argument_spec.rb
+++ Library/Homebrew/test/rubocops/cask/empty_conditional_argument_spec.rb
@@ -4,41 +4,65 @@
 require "rubocops/rubocop-cask"
 
 RSpec.describe RuboCop::Cop::Cask::EmptyConditionalArgument, :config do
-  it "reports an offense when a trailing `arch` argument is an empty string" do
+  it "reports an offense when a trailing argument is an empty string" do
     expect_offense(<<~CASK)
       cask "foo" do
         arch arm: "-arm64", intel: ""
                             ^^^^^^^^^ Remove the empty `intel:` argument from the `arch` stanza.
+        file_arch = on_arch_conditional arm: "-aarch64", intel: ""
+                                                         ^^^^^^^^^ Remove the empty `intel:` argument from the `on_arch_conditional` stanza.
+        os macos: "-darwin", linux: ""
+                             ^^^^^^^^^ Remove the empty `linux:` argument from the `os` stanza.
+        file_os on_system_conditional macos: "-mac", linux: ""
+                                                     ^^^^^^^^^ Remove the empty `linux:` argument from the `on_system_conditional` stanza.
       end
     CASK
 
     expect_correction(<<~CASK)
       cask "foo" do
         arch arm: "-arm64"
+        file_arch = on_arch_conditional arm: "-aarch64"
+        os macos: "-darwin"
+        file_os on_system_conditional macos: "-mac"
       end
     CASK
   end
 
-  it "reports an offense when a leading `arch` argument is an empty string" do
+  it "reports an offense when a leading argument is an empty string" do
     expect_offense(<<~CASK)
       cask "foo" do
         arch arm: "", intel: "-intel"
              ^^^^^^^ Remove the empty `arm:` argument from the `arch` stanza.
+        file_arch = on_arch_conditional arm: "", intel: "-x86_64"
+                                        ^^^^^^^ Remove the empty `arm:` argument from the `on_arch_conditional` stanza.
+        os macos: "", linux: "-linux"
+           ^^^^^^^^^ Remove the empty `macos:` argument from the `os` stanza.
+        file_os on_system_conditional macos: "", linux: "-gnu"
+                                      ^^^^^^^^^ Remove the empty `macos:` argument from the `on_system_conditional` stanza.
       end
     CASK
 
     expect_correction(<<~CASK)
       cask "foo" do
         arch intel: "-intel"
+        file_arch = on_arch_conditional intel: "-x86_64"
+        os linux: "-linux"
+        file_os on_system_conditional linux: "-gnu"
       end
     CASK
   end
 
-  it "reports an offense when every `arch` argument is an empty string" do
+  it "reports an offense when every argument is an empty string" do
     expect_offense(<<~CASK)
       cask "foo" do
         arch arm: "", intel: ""
         ^^^^^^^^^^^^^^^^^^^^^^^ Remove the `arch` stanza as all its arguments are empty.
+        file_arch = on_arch_conditional arm: "", intel: ""
+                    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Remove the `on_arch_conditional` stanza as all its arguments are empty.
+        os macos: "", linux: ""
+        ^^^^^^^^^^^^^^^^^^^^^^^ Remove the `os` stanza as all its arguments are empty.
+        file_os = on_system_conditional macos: "", linux: ""
+                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Remove the `on_system_conditional` stanza as all its arguments are empty.
         url "https://example.com/foo.zip"
       end
     CASK
@@ -50,11 +74,17 @@ RSpec.describe RuboCop::Cop::Cask::EmptyConditionalArgument, :config do
     CASK
   end
 
-  it "reports an offense when the only `arch` argument is an empty string" do
+  it "reports an offense when the only argument is an empty string" do
     expect_offense(<<~CASK)
       cask "foo" do
         arch arm: ""
         ^^^^^^^^^^^^ Remove the `arch` stanza as all its arguments are empty.
+        file_arch = on_arch_conditional intel: ""
+                    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Remove the `on_arch_conditional` stanza as all its arguments are empty.
+        os macos: ""
+        ^^^^^^^^^^^^ Remove the `os` stanza as all its arguments are empty.
+        file_os = on_system_conditional linux: ""
+                  ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ Remove the `on_system_conditional` stanza as all its arguments are empty.
         url "https://example.com/foo.zip"
       end
     CASK
@@ -66,25 +96,37 @@ RSpec.describe RuboCop::Cop::Cask::EmptyConditionalArgument, :config do
     CASK
   end
 
-  it "reports an offense without crashing when an `arch` argument key is not a literal" do
+  it "reports an offense without crashing when an argument key is not a literal" do
     expect_offense(<<~CASK)
       cask "foo" do
         arch arm: "-arm64", some_method => ""
                             ^^^^^^^^^^^^^^^^^ Remove the empty `some_method:` argument from the `arch` stanza.
+        file_arch = on_arch_conditional intel: "-x86_64", some_method => ""
+                                                          ^^^^^^^^^^^^^^^^^ Remove the empty `some_method:` argument from the `on_arch_conditional` stanza.
+        os macos: "-darwin", some_method => ""
+                             ^^^^^^^^^^^^^^^^^ Remove the empty `some_method:` argument from the `os` stanza.
+        file_os = on_system_conditional linux: "-gnu", some_method => ""
+                                                       ^^^^^^^^^^^^^^^^^ Remove the empty `some_method:` argument from the `on_system_conditional` stanza.
       end
     CASK
 
     expect_correction(<<~CASK)
       cask "foo" do
         arch arm: "-arm64"
+        file_arch = on_arch_conditional intel: "-x86_64"
+        os macos: "-darwin"
+        file_os = on_system_conditional linux: "-gnu"
       end
     CASK
   end
 
-  it "reports no offenses when no `arch` argument is an empty string" do
+  it "reports no offenses when no argument is an empty string" do
     expect_no_offenses(<<~CASK)
       cask "foo" do
         arch arm: "-arm64", intel: "-intel"
+        file_arch = on_arch_conditional arm: "-aarch64", intel: "-x86_64"
+        os macos: "-darwin", linux: "-linux"
+        fle_os = on_system_conditional macos: "-mac", linux: "-gnu"
       end
     CASK
   end
