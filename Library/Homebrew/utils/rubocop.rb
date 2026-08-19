ad9b96daa9a09ffe1d0f45a56d8a25ecd02aa3e2
Patrick Linnane <patrick@linnane.io>
2026-08-19 09:20:32 -0700

    utils/rubocop: activate installed platform Rubydex gem
    Signed-off-by: Patrick Linnane <patrick@linnane.io>

--- Library/Homebrew/utils/rubocop.rb
+++ Library/Homebrew/utils/rubocop.rb
@@ -5,6 +5,16 @@
 require_relative "../standalone"
 require_relative "../warnings"
 
+# The standalone bundle is generated on macOS, so activate the installed platform-specific Rubydex gem.
+rubydex_spec = begin
+  Gem::Specification.find_by_name("rubydex")
+rescue Gem::MissingSpecError
+  nil
+end
+if rubydex_spec&.full_require_paths&.none? { |path| $LOAD_PATH.include?(path) }
+  rubydex_spec.activate
+end
+
 Warnings.ignore :parser_syntax do
   require "rubocop"
 end
